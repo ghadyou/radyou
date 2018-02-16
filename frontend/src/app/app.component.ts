fb4fac60d1337dc1c61fc6112b4566bf91659768
aziouiz zakaria <zaziouiz@pictet-technologies.com>
2018-02-16 23:47:13 +0100

    Added footer and header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'radyou-app',
   template: `
+    <radyou-app-header></radyou-app-header>
     <router-outlet></router-outlet>
+    <radyou-app-footer></radyou-app-footer>
   `,
 })
 export class AppComponent {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
-import {appRoutingProviders, routes, routing} from './routes';
-import {RouterModule} from '@angular/router';
+import {appRoutingProviders, routing} from './routes';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    AppFooterComponent,
+    AppHeaderComponent
+  ],
   imports: [
     BrowserModule,
     routing,
     MDBBootstrapModule.forRoot(),
 
   ],
-  declarations: [
-  AppComponent
-],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
