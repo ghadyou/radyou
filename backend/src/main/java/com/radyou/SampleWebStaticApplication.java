31eb1577dee2d39f2e7d0c55f952c7aa825a2fa2
aziouiz zakaria <zaziouiz@pictet-technologies.com>
2018-02-16 16:55:32 +0100

    Added spring boot app annotation.

--- backend/src/main/java/com/radyou/SampleWebStaticApplication.java
+++ backend/src/main/java/com/radyou/SampleWebStaticApplication.java
@@ -2,26 +2,15 @@ package com.radyou;
 
 import com.radyou.config.DBConfig;
 import com.radyou.config.NextRTCEndpointConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-
-import java.lang.reflect.Method;
 
 @Configuration
 @EnableAutoConfiguration(exclude = {
@@ -32,54 +21,11 @@ import java.lang.reflect.Method;
 @Import({NextRTCEndpointConfig.class,
         DBConfig.class
 })
+@SpringBootApplication
 public class SampleWebStaticApplication {
-
-    @Value("${rest.api.base.path}")
-    private String restApiBasePath;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(SampleWebStaticApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
-            }
-        };
-    }
-
-    @Bean
-    public WebMvcRegistrationsAdapter webMvcRegistrationsHandlerMapping() {
-        SampleWebStaticApplication application = this;
-        return new WebMvcRegistrationsAdapter() {
-            @Override
-            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-                return new RequestMappingHandlerMapping() {
-
-                    @Override
-                    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
-                        Class<?> beanType = method.getDeclaringClass();
-                        RestController restApiController = beanType.getAnnotation(RestController.class);
-                        if (restApiController != null) {
-                            PatternsRequestCondition apiPattern = new PatternsRequestCondition(application.restApiBasePath)
-                                    .combine(mapping.getPatternsCondition());
-
-                            mapping = new RequestMappingInfo(mapping.getName(), apiPattern,
-                                    mapping.getMethodsCondition(), mapping.getParamsCondition(),
-                                    mapping.getHeadersCondition(), mapping.getConsumesCondition(),
-                                    mapping.getProducesCondition(), mapping.getCustomCondition());
-                        }
-
-                        super.registerHandlerMethod(handler, method, mapping);
-                    }
-                };
-            }
-        };
-    }
-
 }
 
 
